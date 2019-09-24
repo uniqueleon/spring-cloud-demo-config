0f074caefc6742352137a197d1cf5eb3f2588456
root <root@alog.com>
2019-09-24 16:54:35 +0800

    提交

---
+++ spring-cloud-config-server/src/main/java/org/aztec/spring/cloud/config/server/App.java
@@ -0,0 +1,28 @@
+package org.aztec.spring.cloud.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableConfigServer
+//@RestController
+public class App {
+
+  //@Value("${config.name}")
+  String name = "World";
+
+  //@RequestMapping("/")
+  public String home() {
+    return "Hello " + name;
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(App.class, args);
+  }
+
+}
+
+
